e4b022a3f49c76e5f42e6911954e4a14abc534b5
vinayak <vinayaks0031@gmail.com>
2022-01-19 17:41:34 +0530

    Removed extra permissions

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,10 @@
-document.addEventListener('mouseup', function (e) { 
+document.addEventListener('mouseup', function (e) {
 	e.preventDefault();
 	var sel = window.getSelection().toString(); //storing the word that we've selected
 	if (sel.length > 0) {
-		    chrome.runtime.sendMessage({ word: sel },(response)=>{ //sending this word to -> background.js
-                console.log(response.text); //getting some response from background.js
-		    });
+		chrome.runtime.sendMessage({ word: sel }, (response) => { //sending this word to -> background.js
+			response.text; //getting some response from background.js
+		});
 	}
 	sel = "";
 });
